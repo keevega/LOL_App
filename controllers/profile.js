f6858c022bbeadad376c83a61f1e6b59ee462418
keevega <keevega01@gmail.com>
2026-03-19 16:29:10 -0600

    Refactor API key handling to use environment variable and update .gitignore to exclude .env file

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,6 @@
 import obtenerDatos from "../helpers/getData.js";
 
-const apiKey = 'RGAPI-42917b27-b1e1-4633-92c9-5817978498f0'
+const apiKey = process.env.APIKEY
 
 const show_profile = (req, res) => {
     res.render('home', {
@@ -13,8 +13,6 @@ const search_profile = async (req, res) => {
     const [user,tag] = summonerName.split('#')
 
     const datos = await obtenerDatos(user,tag, apiKey)
-
-    console.log(datos.length)
 
     if(datos.length !== 7){
         res.render('home',{
